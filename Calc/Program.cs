d1773c6cf6689c85c4d5e4f0d310c0efa03e52a5
kamkaz12 <mariusz.drobot@accenture.com>
2019-03-21 09:45:01 +0100

    Add project files.

---
+++ Calc/Program.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the amount of celsius: ");
+            int celsius = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Kelvin = {0}", ConvertToKelvin(celsius));
+            Console.WriteLine("Fahrenheit = {0}", ConvertToFahrenheit(celsius));
+
+            User testUser = new User("test", "1234", "eaaa@aa.aa", 521421323);
+
+            String userToJson = Json.Net.JsonNet.Serialize(testUser);
+            Console.WriteLine(userToJson);
+        }
+
+        public static int ConvertToKelvin(int value)
+        {
+            return value + 273;
+        }
+
+        public static int ConvertToFahrenheit(int value)
+        {
+            return value * 18 / 10 + 32;
+        }
+    }
+
+    public class User
+    {
+        public string login;
+        public string password;
+        public string email;
+        public int phoneNumber;
+
+        public User(string login, string password, string email, int phoneNumber)
+        {
+            this.login = login;
+            this.password = password;
+            this.email = email;
+            this.phoneNumber = phoneNumber;
+        }
+
+    }
+}
---
+++ CalcTests1/ProgramTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void ConvertToKelvinTest()
+        {
+            Assert.AreEqual(283, Program.ConvertToKelvin(10));
+       //     Assert.AreEqual(100, Program.ConvertToKelvin(15));
+        }
+
+        [TestMethod()]
+        public void ConvertToFahrenheitTest()
+        {
+            Assert.AreEqual(50, Program.ConvertToFahrenheit(10));
+        }
+
+    }
+}
